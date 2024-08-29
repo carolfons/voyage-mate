b0824eb25a64b06a6fd4bf940d0c16e9b38bad0d
CarolFons <carolsantosfonseca@outlook.com>
2024-08-29 19:13:46 -0300

    :wrench: Config | Adding first test

---
+++ src/main/routes/trips_routes.py
@@ -0,0 +1,9 @@
+from flask import jsonify, Blueprint
+
+trips_routes_bp = Blueprint("trip_routes", __name__)
+
+
+#criando rotas
+@trips_routes_bp.route("/trips", methods = ["POST"])
+def create_trip():
+    return jsonify({"hello":"world"}),200
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,3 +1,7 @@
 from flask import Flask
+from src.main.routes.trips_routes import trips_routes_bp
 
 app = Flask (__name__) #criando um servidor com Flask
+
+#cadastrando a rota no servidor
+app.register_blueprint(trips_routes_bp)
